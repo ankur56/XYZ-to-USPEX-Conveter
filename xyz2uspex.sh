08dfb36d827a1906c2fc9bed7dca8c840921fa15
ankur56 <ankuriiserm@gmail.com>
2022-07-12 18:34:40 -0700

    Update xyz2uspex.sh

--- xyz2uspex.sh
+++ xyz2uspex.sh
@@ -5,13 +5,14 @@
 #Author: Ankur Gupta, Lawrence Berkeley National Laboratory, 2022
 
 filename="${1%.*}"
-obabel $1 -O ${filename}.fh
+obabel $1 -O ${filename}_centered.xyz -c
+obabel ${filename}_centered.xyz -O ${filename}.fh
 
 tail -n+6 ${filename}.fh | awk '{printf "%3d %3d %3d %s\n", $2,$4,$6,"  0"}' > ${filename}_zmat.txt
 
 sed -i -e "1s/^/  0   0   0   1\n  1   0   0   1\n  2   1   0   1\n/g" ${filename}_zmat.txt
 
-tail -n+3 $1 > ${filename}_temp.xyz
+tail -n+3 ${filename}_centered.xyz > ${filename}_temp.xyz
 
 paste ${filename}_temp.xyz ${filename}_zmat.txt > ${filename}_uspex.txt
 
@@ -19,6 +20,7 @@ nl=$(cat ${filename}_uspex.txt | wc -l)
 
 sed -i -r -e "1s/^/MOL: ${filename}\nNumber of atoms: ${nl}\n/g" ${filename}_uspex.txt 
 
+rm ${filename}_centered.xyz
 rm ${filename}.fh
 rm ${filename}_zmat.txt
 rm ${filename}_temp.xyz
